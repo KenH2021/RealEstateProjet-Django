58535939119d5f5648bb83d28fb5525aa0c7ece0
KenH2021 <91165485+KenH2021@users.noreply.github.com>
2022-03-15 16:02:36 -0400

    Added local setting import

--- btre/settings.py
+++ btre/settings.py
@@ -152,3 +152,8 @@ MESSAGE_TAGS = {
 # EMAIL_HOST_USER ='nekmedia@gmail.com'
 # EMAIL_HOST_PASSWORD ='knbest07'
 # EMAIL_USE_TLS =True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
